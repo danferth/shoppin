ed528377283553fba2dd33002b0bfcf4001de0af
Dan Klotz <danferth@gmail.com>
2018-10-18 19:38:53 +0000

    most of the basic logic in place

--- assets/dev/js/site.js
+++ assets/dev/js/site.js
@@ -7,6 +7,129 @@ $(document).foundation();
 //doc ready
 $(document).ready(function(){
 
+
+//is there a tally record?
+
+//if so populate list
+
+//function to get tally if is exsist and ad them up to get the total so far
+var getTally = function(array){
+  var hold = 0;
+  for(var i = 0; i<array.length; i++){
+    hold = hold + parseInt(array[i]);
+  }
+  $('.tally-input').attr('placeholder', hold);
+  $('.tally-input').blur().val('');
+  return hold;
+};
+
+
+localforage.keys().then(function(keys){
+  if(keys == 'tally'){
+    localforage.getItem('tally').then(function(tally){
+      getTally(tally);
+      console.log(tally);
+    });
+  }
+}).catch(function(err){
+  console.log(err);
+});
+
+
+
+
+
+
+
+
+$('.tally-add').on('click', function(e){
+
+  if($('.tally-input').val()){
+  var iv = $('.tally-input').val();
+  
+  localforage.keys().then(function(keys){
+    if(keys == 'tally'){
+      console.log('tally exsist');
+      localforage.getItem('tally').then(function(item){
+        item.push(iv);
+        getTally(item);
+        localforage.setItem('tally', item).then(function(value){
+          console.log(value);
+        }).catch(function(err){
+          console.log(err);
+        });
+      }).catch(function(err){
+        console.log(err);
+      });
+    }else{
+      localforage.setItem('tally', [iv]).then(function(value){
+        console.log(value);
+        getTally(value);
+        console.log('tally did not exsist');
+      }).catch(function(err){
+        console.log(err);
+      });
+    }
+  }).catch(function(err){
+      console.log(err);
+    });
+  }else{
+    console.log('tally-input was empty');
+  }
+
+});
+
+
+
+$('.tally-clear-list').on('click', function(e){
+  localforage.clear().then(function(){
+    console.log('db clear');
+    $('.tally-input').blur().val('').attr('placeholder', '0');
+  }).catch(function(err){
+    console.log(err);
+  });
+});
+  
+  
+  
+  
+  
+$('.tally-last').on('click', function(e){
+  swal({
+    title: 'remove last entry?',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'yes!'
+  }).then(function(result){
+    if(result.value){
+      localforage.getItem('tally').then(function(tally){
+        tally.pop();
+        localforage.setItem('tally', tally).then(function(){
+          localforage.getItem('tally').then(function(tally){
+            getTally(tally);
+          }).catch(function(err){
+            console.log(err);
+          });
+        }).catch(function(err){
+            console.log(er);
+          });
+        }).catch(function(err){
+          console.log(err);
+      });
+    }
+  });
+  
+}); 
+
+
+
+
+
+
+
+
+
+
 }); //END Doc.ready
 
 
--- index.php
+++ index.php
@@ -7,10 +7,27 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/build/scaffold/head.php';
 ?>
 
 <!-- START -->
-<div class="page-wrap grid-x">
+<div class="page-wrap grid-y">
+  
   <div class="cell small-12">
-    <h1>start</h1>
-    <p>This is in a page wrap <i class="fa fa-smile-o"></i></p>
+    <h1>tally.enter</h1>
+    <input class="tally-input" type="number" pattern="[0-9]*" name="tally" placeholder="0" novalidate/>
+    <ul class="tally-nav button-group">
+      <li><button class="button tally-last"><i class="fa fa-reply"></i></button></li>
+      <li><button class="button tally-add"><i class="fa fa-plus"></i></button></li>
+      <li><button class="button tally-list"><i class="fa fa-list"></i></button></li>
+    </ul>
+  </div>
+  
+  <div class="cell small-12">
+    <h1>tally.list</h1>
+    <button class="button tally-clear-list"><i class="fa fa-remove"></i><i class="fa fa-list"></i></button>
+    <ul class="tally-list">
+      <li>
+        <span class="tally-item">10</span>
+        <button class="button tally-remove"><i class="fa fa-remove"></i></button>
+      </li>
+    </ul>
   </div>
 </div>
 <!-- END -->
